adae5fc9ae8f5a4e0dff501614d44d4a8cdd226c
Evan Kysley <evan@kysley.com>
2024-08-19 12:54:04 -0400

    chore: add test water cursor

--- apps/web/app/utils/cursors.ts
+++ apps/web/app/utils/cursors.ts
@@ -2,6 +2,7 @@ const cursorLookup: Record<string, string> = {
   "0": "/lava_placeholder.gif",
   "1": "/bush_placeholder.gif",
   "2": "/kirby_placeholder.webp",
+  "3": "/water_placeholder.gif",
 };
 
 export { cursorLookup };
